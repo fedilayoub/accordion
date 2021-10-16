dbdd60e49399a8a1d03cb4ab81dbfb32932b726f
fedilayoub <ayoubzahi2016@gmail.com>
2021-10-16 11:57:23 +0100

    accordion

---
+++ README.md
@@ -0,0 +1,4 @@
+# Accordion section  
+I used useState react hook to display or not a set of questions and answers 
+# Demo 
+[Accordion](https://fedilayoub.github.io/accordion)
---
+++ src/Question.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+const Question = ({title , info}) => {
+  const [showInfo,setShowInfo] = useState(false)
+  return <article className="question">
+<header>
+  <h4>{title}</h4>
+  <buttton className="btn" onClick={()=>setShowInfo(!showInfo) } > {showInfo ? <AiOutlineMinus/>:<AiOutlinePlus />}</buttton>
+</header>
+{showInfo && <p>{info}</p>}
+  </article>;
+};
+
+export default Question;
