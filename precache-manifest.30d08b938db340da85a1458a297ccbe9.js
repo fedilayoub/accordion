e119eb1aabd8d9892a92c396624b90653ea46a03
fedilayoub <ayoubzahi2016@gmail.com>
2021-10-16 12:33:36 +0100

    Updates

---
+++ precache-manifest.30d08b938db340da85a1458a297ccbe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf272bc741c51bb01021b1775df77af",
+    "url": "/accordion/index.html"
+  },
+  {
+    "revision": "e33827668c24fc548ea8",
+    "url": "/accordion/static/css/main.5124f360.chunk.css"
+  },
+  {
+    "revision": "714215ef353bcdb2da2b",
+    "url": "/accordion/static/js/2.cf7dc66f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/accordion/static/js/2.cf7dc66f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e33827668c24fc548ea8",
+    "url": "/accordion/static/js/main.40cf32ec.chunk.js"
+  },
+  {
+    "revision": "0af22f8fa2608fd03e14",
+    "url": "/accordion/static/js/runtime-main.fdc8c225.js"
+  }
+]);
